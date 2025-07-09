104d4d3c79d5fdbae096d426cde6f290f85d8b56
Maxpwer222 <cheidb2@gmail.com>
2025-07-08 19:06:23 -0500

    ...

--- js/main.js
+++ js/main.js
@@ -6,32 +6,40 @@ function showSection(id) {
     .querySelectorAll(".nav-link")
     .forEach((link) => link.classList.remove("active"));
   document.getElementById(id).classList.add("active");
-  document.querySelector(`.nav-link[onclick*='${id}']`).classList.add("active");
+  document.querySelector(`.nav-link[data-section='${id}']`).classList.add("active");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Add event listeners for navigation
+  document.querySelectorAll(".nav-link").forEach(function (link) {
+    link.addEventListener("click", function () {
+      const sectionId = this.getAttribute("data-section");
+      showSection(sectionId);
+    });
+  });
+
+
   document.querySelectorAll(".inventory-preview").forEach(function (img) {
     img.addEventListener("click", function () {
       var largeImg = this.getAttribute("data-img");
       document.getElementById("inventoryModalImg").src = largeImg;
     });
   });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Inventory Tracker already handled
   document.querySelectorAll(".ccl-preview").forEach(function (img) {
     img.addEventListener("click", function () {
       var largeImg = this.getAttribute("data-img");
       document.getElementById("cclModalImg").src = largeImg;
     });
   });
+
   document.querySelectorAll(".mpage-preview").forEach(function (img) {
     img.addEventListener("click", function () {
       var largeImg = this.getAttribute("data-img");
       document.getElementById("mpageModalImg").src = largeImg;
     });
   });
+
   document.querySelectorAll(".rrd-preview").forEach(function (img) {
     img.addEventListener("click", function () {
       var largeImg = this.getAttribute("data-img");
